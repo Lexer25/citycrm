6f60d1d89f906eb951f7ebdff49b68a769e2286d
Andre <andrey.buharov@gmail.com>
2024-04-01 10:16:14 +0300

    ДОбавил модули

---
+++ modules/tuner/i18n/ru/ru.php
@@ -0,0 +1,11 @@
+<?php
+
+return array
+(
+	'setting.orgname'						=> 'Название объекта',
+	'setting.iphost'						=> 'IP адрес сервера СКУД',
+	'setting.lic'						=> 'Номер лицензии',
+	'setting.odbcname'						=> 'Источник данных ODBC',
+	'setting.sysVer'						=> 'Версия продукта',
+  
+);
